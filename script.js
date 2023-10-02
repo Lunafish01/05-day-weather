e644f6b8bc2beffc8a06ad1d476f4f28a328f9b4
Lunafish01 <142092354+Lunafish01@users.noreply.github.com>
2023-10-02 01:58:15 -0500

    updated script file

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var apiKey = "dbaf245631a513f4397117d9b90295b3";
+var locationInfo = document.querySelector(".location-info");
+var searchButton = document.querySelector(".searchBtn");
+var currentWeatherEl = document.querySelector(".current-weather");
+var weatherCardseEl = document.querySelector(".weather-cards");
+
+
